4704ba213c946a65b907c26de92a9949311c6b25
Sohail Lajevardi <doxigo@gmail.com>
2024-02-15 12:42:32 +0330

    Add init import to SCSS files generated

--- cli.js
+++ cli.js
@@ -1,31 +1,31 @@
 #!/usr/bin/env node
-const path = require('path');
-const fs = require('fs-extra');
-const yaml = require('js-yaml');
-const prompts = require('@clack/prompts');
+const path = require("path");
+const fs = require("fs-extra");
+const yaml = require("js-yaml");
+const prompts = require("@clack/prompts");
 const { isCancel, cancel, select, text, outro } = prompts;
 
-let radixComponentsPath = '../../contrib/radix/components';
+let radixComponentsPath = "../../contrib/radix/components";
 const activeThemePath = process.cwd();
 
 async function listComponents() {
 	try {
 		const components = await fs.readdirSync(radixComponentsPath);
-		console.log('Available Radix Components:');
+		console.log("Available Radix Components:");
 		for (const componentDir of components) {
 			const ymlPath = `${radixComponentsPath}/${componentDir}/${componentDir}.component.yml`;
 			if (fs.existsSync(ymlPath)) {
-				const fileContents = await fs.readFile(ymlPath, 'utf8');
+				const fileContents = await fs.readFile(ymlPath, "utf8");
 				const doc = yaml.load(fileContents);
 				console.log(`- ${doc.name}: ${doc.description}`);
 			}
 		}
 
 		outro(
-			"That's all we have for now, To add a component to your theme, run `drupal-radix-cli add` or to generate a new one `drupal-radix-cli generate`."
+			"That's all we have for now, To add a component to your theme, run `drupal-radix-cli add` or to generate a new one `drupal-radix-cli generate`.",
 		);
 	} catch (error) {
-		console.error('Error listing components:', error);
+		console.error("Error listing components:", error);
 	}
 }
 
@@ -36,7 +36,7 @@ async function addComponent() {
 			.map((component) => {
 				const ymlPath = `${radixComponentsPath}/${component}/${component}.component.yml`;
 				if (fs.existsSync(ymlPath)) {
-					const doc = yaml.load(fs.readFileSync(ymlPath, 'utf8'));
+					const doc = yaml.load(fs.readFileSync(ymlPath, "utf8"));
 					return { value: component, label: doc.name };
 				}
 				return null;
@@ -45,17 +45,17 @@ async function addComponent() {
 
 		const maxItems = 10;
 		const componentName = await select({
-			message: 'Pick a Radix component to add to your theme.',
+			message: "Pick a Radix component to add to your theme.",
 			options: options,
 			maxItems: maxItems,
 			onCancel: () => {
-				cancel('Operation cancelled.');
+				cancel("Operation cancelled.");
 				process.exit(1);
 			},
 		});
 
 		if (isCancel(componentName)) {
-			cancel('Operation cancelled.');
+			cancel("Operation cancelled.");
 			process.exit(1);
 		}
 
@@ -69,34 +69,34 @@ async function addComponent() {
 		fs.copySync(sourcePath, targetPath);
 		outro(`Component ${componentName} has been added at: ${targetPath}`);
 	} catch (error) {
-		console.error('Error during the add component process:', error);
+		console.error("Error during the add component process:", error);
 	}
 }
 
 async function generateComponent() {
 	const response = await text({
-		message: 'What is the name of your component?',
-		placeholder: 'card',
+		message: "What is the name of your component?",
+		placeholder: "card",
 		onCancel: () => {
-			cancel('Operation cancelled.');
+			cancel("Operation cancelled.");
 			process.exit(1);
 		},
 	});
 
 	if (isCancel(response)) {
-		cancel('Operation cancelled.');
+		cancel("Operation cancelled.");
 		process.exit(1);
 	}
 
 	const componentName = response;
 
-	const componentsDirPath = path.join(activeThemePath, 'components');
+	const componentsDirPath = path.join(activeThemePath, "components");
 	const componentDirPath = path.join(componentsDirPath, componentName);
 
 	try {
 		if (await fs.pathExists(componentDirPath)) {
 			outro(
-				`The ${componentName} component already exists, maybe try another name?`
+				`The ${componentName} component already exists, maybe try another name?`,
 			);
 			process.exit(1);
 		}
@@ -108,7 +108,7 @@ async function generateComponent() {
 			`${componentName}.twig`,
 			`${componentName}.scss`,
 			`_${componentName}.js`,
-			'README.md',
+			"README.md",
 			`${componentName}.component.yml`,
 		];
 
@@ -117,11 +117,14 @@ async function generateComponent() {
 			await fs.ensureFile(filePath);
 		}
 
+		const scssFilePath = path.join(componentDirPath, `${componentName}.scss`);
+		await fs.appendFile(scssFilePath, '@import "../../src/scss/init";\n');
+
 		console.log(
-			`Component ${componentName} generated successfully. Make sure to remove anything that you don't need and update your ${componentName}.component.yml file.`
+			`DA FOCKIN Component ${componentName} generated successfully. Make sure to remove anything that you don't need and update your ${componentName}.component.yml file.`,
 		);
 	} catch (error) {
-		console.error('Error generating component:', error);
+		console.error("Error generating component:", error);
 		process.exit(1);
 	}
 }
@@ -145,17 +148,17 @@ You can also use the --help flag to show this help message.
 
 (async () => {
 	const args = process.argv.slice(2);
-	const radixPathFlagIndex = args.findIndex((arg) => arg === '--radix-path');
+	const radixPathFlagIndex = args.findIndex((arg) => arg === "--radix-path");
 
 	if (radixPathFlagIndex > -1) {
 		let providedPath = args[radixPathFlagIndex + 1];
 		if (!providedPath) {
 			try {
 				providedPath = await text({
-					message: 'Enter the path to the Radix components directory:',
-					placeholder: '../../../contrib/radix/components',
+					message: "Enter the path to the Radix components directory:",
+					placeholder: "../../../contrib/radix/components",
 					onCancel: () => {
-						cancel('Operation cancelled.');
+						cancel("Operation cancelled.");
 						process.exit(1);
 					},
 				});
@@ -163,10 +166,10 @@ You can also use the --help flag to show this help message.
 				radixComponentsPath = path.resolve(providedPath);
 			} catch (error) {
 				if (isCancel(error)) {
-					cancel('Operation cancelled.');
+					cancel("Operation cancelled.");
 					process.exit(1);
 				} else {
-					console.error('An unexpected error occurred:', error);
+					console.error("An unexpected error occurred:", error);
 					process.exit(1);
 				}
 			}
@@ -175,23 +178,23 @@ You can also use the --help flag to show this help message.
 		}
 	}
 
-	const command = args[0] || 'help';
+	const command = args[0] || "help";
 
 	try {
-		if (command === 'list') {
+		if (command === "list") {
 			await listComponents();
-		} else if (command === 'add') {
+		} else if (command === "add") {
 			await addComponent();
-		} else if (command === 'generate') {
+		} else if (command === "generate") {
 			await generateComponent();
-		} else if (command === 'help') {
+		} else if (command === "help") {
 			showHelp();
 		} else {
 			console.log(`Unknown command: ${command}`);
 			showHelp();
 		}
 	} catch (error) {
-		console.error('An error occurred:', error);
+		console.error("An error occurred:", error);
 		process.exit(1);
 	}
 })();
